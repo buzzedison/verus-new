7a631756fcae451d7b2faf28ec5062e79e04968d
Edison <39317995+buzzedison@users.noreply.github.com>
2022-04-28 23:25:02 +0000

    added files via upload

---
+++ navbar.js
@@ -0,0 +1,12 @@
+const activePage = window.location.pathname;
+// console.log(activePage);
+
+const navLinks = document.querySelectorAll('nav a').
+forEach(link => {
+    console.log(link.href);
+    if(link.href.includes(`${activePage}`)){
+      
+        link.classList.add('active');
+    }
+})
+
